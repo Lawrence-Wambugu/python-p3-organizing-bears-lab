f64561b5ff97aedb4a69685fbed29c26d3aebe3e
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-05-26 09:25:59 +0300

    pass lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 10, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'white', 'friendly', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'strict', 0),
+('Melissa', 13, 'F', 'black', 'gentle', 1),
+('Grinch', 2, 'M', 'green', 'grumpy', 1),
+('Wendy', 6, 'F', 'white', 'playful', 1),
+(NULL, 7, 'M', 'grey', 'mysterious', 0);
